21f8051978c738ff5a004987cf55c196c45a735e
Luiz Gustavo Vèras <gustavo_veras@ifsp.edu.br>
2025-10-30 11:21:20 -0300

    Adição da Var de Ambiente PORT

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from app.routes.book_routes import book_bp
 from app.routes.author_routes import author_bp
@@ -11,4 +12,4 @@ app.register_blueprint(book_bp, url_prefix="/books")
 app.register_blueprint(author_bp, url_prefix="/authors")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port = os.environ.get('PORT', 3000), debug=True)
